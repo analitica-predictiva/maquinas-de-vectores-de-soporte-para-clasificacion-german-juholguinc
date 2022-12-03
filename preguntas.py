9fa8e2d76b36e04f737fc172af7fe61939a2fbf7
juan holguin <juholguinc@unal.edu.co>
2022-12-03 07:28:09 -0500

    p2

--- preguntas.py
+++ preguntas.py
@@ -173,23 +173,18 @@ def pregunta_02():
     """
     Preparación del dataset.
     """
-
-    # Importe train_test_split
-    from ____ import ____
+    from sklearn.model_selection import train_test_split
 
     # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
     X, y = pregunta_01()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 123. Use 100 patrones para la muestra de prueba.
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
-    )
+    (X_train, X_test, y_train, y_test,) = train_test_split(
+        X, y, test_size=100, random_state=123, )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
+    print(len(X_train), len(X_test), len(y_train), len(y_test))
     return X_train, X_test, y_train, y_test
 
 
